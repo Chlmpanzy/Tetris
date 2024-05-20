3395b50e2842ad94ab1f4d2d85cebb83defcdad2
Paul <133292312+Chlmpanzy@users.noreply.github.com>
2024-05-20 15:10:37 -0400

    Fixed shadow and drawing border

--- Tetris Game/Classes.py
+++ Tetris Game/Classes.py
@@ -1,65 +1,75 @@
 import pygame
 
-BLACK     = (  0,  0,  0)                       
-RED         = (255,  0,  0)                     
-GREEN    = (  0,255,  0)                     
-BLUE       = (  0,  0,255)                     
-ORANGE = (255,127,  0)               
-CYAN      = (  0,183,235)                   
-MAGENTA = (255,  0,255)                   
-YELLOW    = (255,255,  0)
-WHITE        = (255,255,255) 
-COLOURS  = [ BLACK,  RED,  GREEN,  BLUE,  ORANGE,  CYAN,  MAGENTA,  YELLOW,  WHITE ]
-CLRNames = ['black','red','green','blue','orange','cyan','magenta','yellow','white']
-figures        = [  None , 'Z' ,  'S'  ,  'J' ,  'L'   ,  'I' ,   'T'   ,   'O'  , None]
-COLUMNS = 10                            
-ROWS = 22                            
-LEFT = 9                                
-TOP = 1                                 
-MIDDLE = LEFT + COLUMNS//2               
-RIGHT = LEFT + COLUMNS                  
-BOTTOM = TOP + ROWS   
+BLACK = (0, 0, 0)
+GRAY = (72, 72, 72)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+ORANGE = (255, 127, 0)
+CYAN = (0, 183, 235)
+MAGENTA = (255, 0, 255)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+COLOURS = [BLACK, RED, GREEN, BLUE, ORANGE, CYAN, MAGENTA, YELLOW, GRAY]
+CLRNames = [
+    'black', 'red', 'green', 'blue', 'orange', 'cyan', 'magenta', 'yellow',
+    'white'
+]
+figures = [None, 'Z', 'S', 'J', 'L', 'I', 'T', 'O', None]
 
-class Block(object):                    
+COLUMNS = 10
+ROWS = 22
+LEFT = 9
+TOP = 1
+MIDDLE = LEFT + COLUMNS // 2
+RIGHT = LEFT + COLUMNS
+BOTTOM = TOP + ROWS
+
+class Block(object):
     """ A square - basic building block
         data:               behaviour:
             col - column        move left/right/up/down
             row - row           draw
             clr - colour
     """
-    def __init__(self, col = 1, row = 1, clr = 1):
-        self.col = col                  
-        self.row = row                  
+
+    def __init__(self, col=1, row=1, clr=1, shadow=False):
+        self.col = col
+        self.row = row
         self.clr = clr
+        self.shadow = shadow
 
-    def __str__(self):                  
-        return '('+str(self.col)+','+str(self.row)+') '+CLRNames[self.clr]
+    def __str__(self):
+        return '(' + str(self.col) + ',' + str(
+            self.row) + ') ' + CLRNames[self.clr]
 
-    def draw(self, surface, gridsize=20):                     
-        x = self.col * gridsize        
+    def draw(self, surface, gridsize=20):
+        x = self.col * gridsize
         y = self.row * gridsize
-        CLR = COLOURS[self.clr]
-        pygame.draw.rect(surface,CLR,(x,y,gridsize,gridsize), 0)
-        pygame.draw.rect(surface, WHITE,(x,y,gridsize+1,gridsize+1), 1)
 
+        CLR = COLOURS[-1] if self.shadow else COLOURS[self.clr]
+        
+        pygame.draw.rect(surface, CLR, (x, y, gridsize, gridsize), 0)
+        pygame.draw.rect(surface, WHITE, (x, y, gridsize + 1, gridsize + 1), 1)
+        
     def __eq__(self, other):
         if self.col == other.col and self.row == other.row:
             return True
         return False
-    
-    def moveUp(self):                  
+
+    def moveUp(self):
         self.row = self.row - 1
-        
-    def moveDown(self):                
+
+    def moveDown(self):
         self.row = self.row + 1
 
-    def moveLeft(self):                
+    def moveLeft(self):
         self.col = self.col - 1
-        
-    def moveRight(self):                
+
+    def moveRight(self):
         self.col = self.col + 1
-        
-#---------------------------------------#
+
+
 class Cluster(object):
     """ Collection of blocks
         data:
@@ -67,175 +77,173 @@ class Cluster(object):
             row - row where the anchor block is located
             blocksNo - number of blocks
     """
-    def __init__(self, col = 1, row = 1, blocksNo = 1):
-        self.col = col                    
-        self.row = row                   
-        self.clr = 0                          
-        self.blocks = [Block()]*blocksNo      
-        self._colOffsets = [0]*blocksNo  
-        self._rowOffsets = [0]*blocksNo  
+
+    def __init__(self, col=1, row=1, blocksNo=1, shadow=False):
+        self.col = col
+        self.row = row
+        self.clr = 0
+        self.shadow = shadow
+        self.blocks = [Block(shadow = self.shadow)] * blocksNo
+        self._colOffsets = [0] * blocksNo
+        self._rowOffsets = [0] * blocksNo
 
     def _update(self):
         for i in range(len(self.blocks)):
-            blockCOL = self.col+self._colOffsets[i] 
-            blockROW = self.row+self._rowOffsets[i] 
+            blockCOL = self.col + self._colOffsets[i]
+            blockROW = self.row + self._rowOffsets[i]
             blockCLR = self.clr
-            self.blocks[i]= Block(blockCOL, blockROW, blockCLR)
+            self.blocks[i] = Block(blockCOL, blockROW, blockCLR, shadow=self.shadow)
 
-    def draw(self, surface, gridsize):                     
+    def draw(self, surface, gridsize):
         for block in self.blocks:
             block.draw(surface, gridsize)
 
     def collides(self, other):
-        """ Compare each block from a cluster to all blocks from another cluster.
-            Return True only if there is a location conflict.
-        """
-
         for i in self.blocks:
             for j in other.blocks:
                 if i == j:
                     return True
         return False
-            
-    def append(self, other): 
-        """ Append all blocks from another cluster to this one.
-        """
 
+    def append(self, other):
         for i in other.blocks:
             self.blocks.append(i)
-            
-#---------------------------------------#
+
 class Obstacles(Cluster):
-    """ Collection of tetrominoe blocks on the playing field, left from previous shapes.
-    """        
-    def __init__(self, col = 0, row = 0, blocksNo = 0):
-        Cluster.__init__(self, col, row, blocksNo)      # initially the playing field is empty(no shapes are left inside the field)
+
+    def __init__(self, col=0, row=0, blocksNo=0):
+        Cluster.__init__(self, col, row, blocksNo)
 
     def show(self):
         print("\nObstacle: ")
         for block in self.blocks:
-            print (block)
+            print(block)
 
     def findFullRows(self, top, lastRow, columns):
         fullRows = []
         rows = []
-        for block in self.blocks:                       
-            rows.append(block.row)    # make a list with only the row numbers of all blocks
-            
-        for row in range(TOP, BOTTOM):                  # starting from the top (row 0), and down to the bottom
-            if rows.count(row) >= columns:            # if the number of blocks with certain row number
-                fullRows.append(row)                      # equals to the number of columns -> the row is full
-        return fullRows                                         # return a list with the full rows' numbers
-
+        for block in self.blocks:
+            rows.append(block.row)
+        for row in range(TOP, BOTTOM):
+            if rows.count(row) >= columns:
+                fullRows.append(row)
+        return fullRows
 
     def removeFullRows(self, fullRows):
-        
-        for row in fullRows:                                 # for each full row, STARTING FROM THE TOP (fullRows are in order)
-            for i in reversed(range(len(self.blocks))): # check all obstacle blocks in REVERSE ORDER,
-                                                                       # so when popping them the index doesn't go out of range !!!
-                if self.blocks[i].row == row:
-                    self.blocks.pop(i) # remove each block that is on this row
-                elif self.blocks[i].row < row:
-                    self.blocks[i].moveDown()         # move down each block that is above this row
-   
-#---------------------------------------#
-class Shape(Cluster):                     
-    """ A tetrominoe in one of the shapes: Z,S,J,L,I,T,O; consists of 4 x Block() objects
-        data:               behaviour:
-            col - column        move left/right/up/down
-            row - row           draw
-            clr - colour        rotate
-                * figure/shape is defined by the colour
-            rot - rotation             
-    """
 
-    def __init__(self, col = 1, row = 1, clr = 1, shadow = False):
-        Cluster.__init__(self, col, row, 4)
+        for row in fullRows:
+            for i in reversed(range(len(self.blocks))):
+                if self.blocks[i].row == row:
+                    self.blocks.pop(i)
+                elif self.blocks[i].row < row:
+                    self.blocks[i].moveDown()
+
+
+#---------------------------------------#
+class Shape(Cluster):
+
+    def __init__(self, col=1, row=1, clr=1, shadow = False):
+        self.shadow = shadow
+        Cluster.__init__(self, col, row, 4, self.shadow)
         self.clr = clr
         self._rot = 1
-        self._colOffsets = [-1, 0, 0, 1] 
-        self._rowOffsets = [-1,-1, 0, 0] 
-        self._rotate() 
-        
-    def __str__(self):                  
-        return figures[self.clr]+' ('+str(self.col)+','+str(self.row)+') '+CLRNames[self.clr]
+        self._colOffsets = [-1, 0, 0, 1]
+        self._rowOffsets = [-1, -1, 0, 0]
+        self._rotate()
 
+    def __str__(self):
+        return figures[self.clr] + ' (' + str(self.col) + ',' + str(
+            self.row) + ') ' + CLRNames[self.clr]
 
     def _rotate(self):
-        """ offsets are assigned starting from the farthest (most distant) block in reference to the anchor block """
-        if self.clr == 1:    #           (default rotation)    
-                             #   o             o o                o              
-                             # o x               x o            x o          o x
-                             # o                                o              o o
-            _colOffsets = [[-1,-1, 0, 0], [-1, 0, 0, 1], [ 1, 1, 0, 0], [ 1, 0, 0,-1]] 
-            _rowOffsets = [[ 1, 0, 0,-1], [-1,-1, 0, 0], [-1, 0, 0, 1], [ 1, 1, 0, 0]]       
+        
+        if self.clr == 1:  #           (default rotation)
+            #   o             o o                o
+            # o x               x o            x o          o x
+            # o                                o              o o
+            _colOffsets = [[-1, -1, 0, 0], [-1, 0, 0, 1], [1, 1, 0, 0],
+                           [1, 0, 0, -1]]
+            _rowOffsets = [[1, 0, 0, -1], [-1, -1, 0, 0], [-1, 0, 0, 1],
+                           [1, 1, 0, 0]]
         elif self.clr == 2:  #
-                             # o                 o o           o              
-                             # o x             o x             x o             x o
-                             #   o                               o           o o
-            _colOffsets = [[-1,-1, 0, 0], [ 1, 0, 0,-1], [ 1, 1, 0, 0], [-1, 0, 0, 1]] 
-            _rowOffsets = [[-1, 0, 0, 1], [-1,-1, 0, 0], [ 1, 0, 0,-1], [ 1, 1, 0, 0]] 
-        elif self.clr == 3:  # 
-                             #   o             o                o o              
-                             #   x             o x o            x           o x o
-                             # o o                              o               o
-            _colOffsets = [[-1, 0, 0, 0], [-1,-1, 0, 1], [ 1, 0, 0, 0], [ 1, 1, 0,-1]] 
-            _rowOffsets = [[ 1, 1, 0,-1], [-1, 0, 0, 0], [-1,-1, 0, 1], [ 1, 0, 0, 0]]             
-        elif self.clr == 4:  #  
-                             # o o                o             o              
-                             #   x            o x o             x           o x o
-                             #   o                              o o         o
-            _colOffsets = [[-1, 0, 0, 0], [-1, 1, 0,1], [0, 0, 0,1], [-1, -1, 0,1]]
-            _rowOffsets = [[-1,-1, 0, 1], [0, 0, 0, -1], [-1,1, 0, 1], [1,0, 0, 0]]
+            # o                 o o           o
+            # o x             o x             x o             x o
+            #   o                               o           o o
+            _colOffsets = [[-1, -1, 0, 0], [1, 0, 0, -1], [1, 1, 0, 0],
+                           [-1, 0, 0, 1]]
+            _rowOffsets = [[-1, 0, 0, 1], [-1, -1, 0, 0], [1, 0, 0, -1],
+                           [1, 1, 0, 0]]
+        elif self.clr == 3:  #
+            #   o             o                o o
+            #   x             o x o            x           o x o
+            # o o                              o               o
+            _colOffsets = [[-1, 0, 0, 0], [-1, -1, 0, 1], [1, 0, 0, 0],
+                           [1, 1, 0, -1]]
+            _rowOffsets = [[1, 1, 0, -1], [-1, 0, 0, 0], [-1, -1, 0, 1],
+                           [1, 0, 0, 0]]
+        elif self.clr == 4:  #
+            # o o                o             o
+            #   x            o x o             x           o x o
+            #   o                              o o         o
+            _colOffsets = [[-1, 0, 0, 0], [-1, 1, 0, 1], [0, 0, 0, 1],
+                           [-1, -1, 0, 1]]
+            _rowOffsets = [[-1, -1, 0, 1], [0, 0, 0, -1], [-1, 1, 0, 1],
+                           [1, 0, 0, 0]]
         elif self.clr == 5:  #   o                              o
-                             #   o                              x              
-                             #   x            o x o o           o          o o x o
-                             #   o                              o              
-            _colOffsets = [[ 0, 0, 0, 0], [ 2, 1, 0,-1], [ 0, 0, 0, 0], [-2,-1, 0, 1]] 
-            _rowOffsets = [[-2,-1, 0, 1], [ 0, 0, 0, 0], [ 2, 1, 0,-1], [ 0, 0, 0, 0]]            
+            #   o                              x
+            #   x            o x o o           o          o o x o
+            #   o                              o
+            _colOffsets = [[0, 0, 0, 0], [2, 1, 0, -1], [0, 0, 0, 0],
+                           [-2, -1, 0, 1]]
+            _rowOffsets = [[-2, -1, 0, 1], [0, 0, 0, 0], [2, 1, 0, -1],
+                           [0, 0, 0, 0]]
         elif self.clr == 6:  #
-                             #   o              o                o              
-                             # o x            o x o              x o         o x o
-                             #   o                               o             o 
-            _colOffsets = [[ 0,-1, 0, 0], [-1, 0, 0, 1], [ 0, 1, 0, 0], [ 1, 0, 0,-1]] 
-            _rowOffsets = [[ 1, 0, 0,-1], [ 0,-1, 0, 0], [-1, 0, 0, 1], [ 0, 1, 0, 0]] 
-        else:                # 
-                             # o o            o o               o o          o o
-                             # o x            o x               o x          o x
-                             # 
-            _colOffsets = [[-1,-1, 0, 0], [-1,-1, 0, 0], [-1,-1, 0, 0], [-1,-1, 0, 0]] 
-            _rowOffsets = [[ 0,-1, 0,-1], [ 0,-1, 0,-1], [ 0,-1, 0,-1], [ 0,-1, 0,-1]] 
-        self._colOffsets = _colOffsets[self._rot] 
-        self._rowOffsets = _rowOffsets[self._rot] 
-        self._update() 
+            #   o              o                o
+            # o x            o x o              x o         o x o
+            #   o                               o             o
+            _colOffsets = [[0, -1, 0, 0], [-1, 0, 0, 1], [0, 1, 0, 0],
+                           [1, 0, 0, -1]]
+            _rowOffsets = [[1, 0, 0, -1], [0, -1, 0, 0], [-1, 0, 0, 1],
+                           [0, 1, 0, 0]]
+        else:  #
+            # o o            o o               o o          o o
+            # o x            o x               o x          o x
+            #
+            _colOffsets = [[-1, -1, 0, 0], [-1, -1, 0, 0], [-1, -1, 0, 0],
+                           [-1, -1, 0, 0]]
+            _rowOffsets = [[0, -1, 0, -1], [0, -1, 0, -1], [0, -1, 0, -1],
+                           [0, -1, 0, -1]]
+        self._colOffsets = _colOffsets[self._rot]
+        self._rowOffsets = _rowOffsets[self._rot]
+        self._update()
 
-    def moveLeft(self):                
-        self.col = self.col - 1                   
-        self._update() 
-        
-    def moveRight(self):               
-        self.col = self.col + 1                   
-        self._update() 
-        
-    def moveDown(self):                
-        self.row = self.row + 1           
-        self._update() 
-        
-    def moveUp(self):                  
-        self.row = self.row - 1                   
-        self._update() 
+    def moveLeft(self):
+        self.col = self.col - 1
+        self._update()
+
+    def moveRight(self):
+        self.col = self.col + 1
+        self._update()
+
+    def moveDown(self):
+        self.row = self.row + 1
+        self._update()
+
+    def moveUp(self):
+        self.row = self.row - 1
+        self._update()
 
     def rotateClkwise(self):
-        self._rot = (self._rot + 1)%4     
+        self._rot = (self._rot + 1) % 4
         self._rotate()
 
     def rotateCntclkwise(self):
-        self._rot = (self._rot - 1)%4     
+        self._rot = (self._rot - 1) % 4
         self._rotate()
 
-    
 
-#---------------------------------------#
+
+
 class Floor(Cluster):
     """ Horizontal line of blocks
         data:
@@ -243,13 +251,14 @@ class Floor(Cluster):
             row - row where the anchor block is located
             blocksNo - number of blocks 
     """
-    def __init__(self, col = 1, row = 1, blocksNo = 1): 
+
+    def __init__(self, col=1, row=1, blocksNo=1):
         Cluster.__init__(self, col, row, blocksNo)
         for i in range(blocksNo):
             self._colOffsets[i] = i
-        self._update() 
-            
-#---------------------------------------#
+        self._update()
+
+
 class Wall(Cluster):
     """ Vertical line of blocks
         data:
@@ -257,13 +266,10 @@ class Wall(Cluster):
             row - row where the anchor block is located
             blocksNo - number of blocks 
     """
-    def __init__(self, col = 1, row = 1, blocksNo = 1):
+
+    def __init__(self, col=1, row=1, blocksNo=1):
         Cluster.__init__(self, col, row, blocksNo)
         for i in range(blocksNo):
-            self._rowOffsets[i] = i 
-        self._update() 
-
-bottom = Floor(LEFT,BOTTOM,COLUMNS)
-lastRow = Floor(LEFT,BOTTOM+1,COLUMNS)
-leftWall = Wall(LEFT-1, TOP, ROWS)
-rightWall = Wall(RIGHT, TOP, ROWS)
+            self._rowOffsets[i] = i
+        self._update()
+    
--- Tetris Game/Tetris.py
+++ Tetris Game/Tetris.py
@@ -1,6 +1,7 @@
 from Classes import *
 from random import randint
 import pygame
+
 pygame.init()
 clock = pygame.time.Clock()
 HEIGHT = 600
@@ -10,21 +11,25 @@ screen=pygame.display.set_mode((WIDTH,HEIGHT))
 GREY = (192,192,192)
 
 #---------------------------------------#
-COLUMNS = 10                          #
-ROWS = 22                                   # 
-LEFT = 9                                        # 
-RIGHT = LEFT + COLUMNS         # 
-MIDDLE = LEFT + COLUMNS//2  #
-TOP = 1                                        #
-FLOOR = TOP + ROWS             #
-shapeNo = randint(1,7)      
-tetra = Shape(MIDDLE-1,2,shapeNo)
-shadow = Shape(MIDDLE-1,2, shapeNo)
-nextShapeNo = randint(1,7)
-nextShape = Shape(LEFT-6, 3, nextShapeNo)
+COLUMNS = 10  #
+ROWS = 22  #
+LEFT = 9  #
+RIGHT = LEFT + COLUMNS  #
+MIDDLE = LEFT + COLUMNS // 2  #
+TOP = 1  #
+FLOOR = TOP + ROWS  #
+
+shapeNo = randint(1, 7)
+tetra = Shape(MIDDLE - 1, 2, shapeNo)
+shadow = Shape(MIDDLE - 1, 2, shapeNo, shadow=True)
+nextShapeNo = randint(1, 7)
+nextShape = Shape(LEFT - 6, 3, nextShapeNo)
 oldShape = shapeNo
 holdShapeNo = 0
 holdShape = Shape(LEFT-6, 12, holdShapeNo)
+
+bottom = Floor(LEFT,BOTTOM,COLUMNS)
+lastRow = Floor(LEFT,BOTTOM+1,COLUMNS)
 floor = Floor(LEFT,FLOOR,COLUMNS)
 leftWall = Wall(LEFT-1, TOP, ROWS)
 rightWall = Wall(RIGHT, TOP, ROWS)
@@ -34,69 +39,57 @@ level = 0
 time = 0
 font = pygame.font.SysFont("Ariel Black", 60) 
 swap = False
+stored = False
 
-#---------------------------------------#
 
-#---------------------------------------#
-#   functions                            #
-#---------------------------------------#
-'''
-def drawGrid():
-    """ Draw horisontal and vertical lines on the entire game window.
-        Space between the lines is GRIDSIZE.
-    """
-    for y in range (0, HEIGHT, GRIDSIZE):
-        pygame.draw.line(screen, WHITE, (TOP,y), (BOTTOM, y), 1)
-
-    for x in range(LEFT*25,RIGHT*25,GRIDSIZE):
-        pygame.draw.line(screen,WHITE, (x,0),(x,0),1)
-'''
 def redrawScreen():
     screen.fill(BLACK)
-    tetra.draw(screen, GRIDSIZE)
     shadow.row = 0
-    while not(shadow.collides(bottom) or shadow.collides(obst)):
+
+    while not (shadow.collides(bottom) or shadow.collides(obst)):
         shadow.moveDown()
     shadow.moveUp()
+
     if shadow.row > tetra.row:
         shadow.draw(screen, GRIDSIZE)
+    tetra.draw(screen, GRIDSIZE)
     for ob in obst.blocks:
         ob.draw(screen, GRIDSIZE)
-    floor.draw(screen, GRIDSIZE)
-    leftWall.draw(screen, GRIDSIZE)
-    rightWall.draw(screen, GRIDSIZE)
+    
     nextShape.draw(screen, GRIDSIZE)
     holdShape.draw(screen, GRIDSIZE)
-    pygame.display.update() 
-    
+    bottom.draw(screen, GRIDSIZE)
+    leftWall.draw(screen, GRIDSIZE)
+    rightWall.draw(screen, GRIDSIZE)
+
+    pygame.display.update()
+
+
 def findBottom(col: int):
     bot = 0
     for ob in obst.blocks:
-        if ob.col == col and bot<ob.row:
+        if ob.col == col and bot < ob.row:
             bot = ob.row
     if bot == 0:
         return 22
     return bot
-        
-#---------------------------------------#
-#   main program                    #
-#---------------------------------------#    
 
-inPlay = True                                         
 
-while inPlay:               
+
+inPlay = True
+
+while inPlay:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:         
+        if event.type == pygame.QUIT:
             inPlay = False
         if event.type == pygame.KEYDOWN:
-            
-            
-                
-                    
-            if event.key == pygame.K_UP:            
+
+            if event.key == pygame.K_UP:
                 tetra.rotateClkwise()
                 shadow.rotateClkwise()
-                if tetra.collides(leftWall) or tetra.collides(rightWall) or tetra.collides(bottom) or tetra.collides(obst):
+                if tetra.collides(leftWall) or tetra.collides(
+                        rightWall) or tetra.collides(bottom) or tetra.collides(
+                            obst):
                     tetra.rotateCntclkwise()
                     shadow.rotateCntclkwise()
 
@@ -106,56 +99,80 @@ while inPlay:
                 if tetra.collides(leftWall) or tetra.collides(obst):
                     tetra.moveRight()
                     shadow.moveRight()
-                    
+
             if event.key == pygame.K_RIGHT:
                 tetra.moveRight()
                 shadow.moveRight()
-                
+
                 if tetra.collides(rightWall) or tetra.collides(obst):
                     tetra.moveLeft()
                     shadow.moveLeft()
             if event.key == pygame.K_c and not swap:
-                tetra, holdShape = Shape(MIDDLE-1,2,nextShapeNo), Shape(LEFT-6, 12, oldShape)
-                #holdShape = nextShapeNo
-                oldShape, nextShapeNo = nextShapeNo, oldShape
-                swap = True
-                
+                if not stored:
+                    tetra, shadow, holdShape = Shape(
+                        MIDDLE - 1, 2, nextShapeNo), Shape(MIDDLE - 1,
+                                                           2,
+                                                           nextShapeNo,
+                                                           shadow=True), Shape(
+                                                               LEFT - 6, 12,
+                                                               oldShape)
+                    oldShape, nextShapeNo = nextShapeNo, oldShape
+                    holdShapeNo = nextShapeNo
+                    swap = True
+                    stored = True
+                else:
+                    tetra, shadow, holdShape = Shape(
+                        MIDDLE - 1, 2, holdShapeNo), Shape(MIDDLE - 1,
+                                                           2,
+                                                           holdShapeNo,
+                                                           shadow=True), Shape(
+                                                               LEFT - 6, 12,
+                                                               oldShape)
+                    oldShape, holdShapeNo = holdShapeNo, oldShape
+                    swap = True
+
             if event.key == pygame.K_SPACE:
-                while not(tetra.collides(bottom) or tetra.collides(obst)):
+                while not (tetra.collides(bottom) or tetra.collides(obst)):
                     tetra.moveDown()
-                
-                
+
             if event.key == pygame.K_DOWN:
                 counter = 0
                 tetra.moveDown()
-    
+
+
     if counter > 1000:
         counter = 0
         tetra.moveDown()
-        
-    if tetra.collides(bottom) or tetra.collides(obst):
-        tetra.moveUp()
-        obst.append(tetra)
-        obst.show()
-        tetra = Shape(MIDDLE-1,2,nextShapeNo)
-        shadow = Shape(MIDDLE-1,2, nextShapeNo)
-        oldShape = nextShapeNo
-        print(oldShape)
-        nextShapeNo = randint(1,7)
-        nextShape = Shape(LEFT-6, 3, nextShapeNo)
-        swap = False        
-        fullRows = obst.findFullRows(TOP, FLOOR, COLUMNS)    # finds the full rows and removes their blocks from the obstacles 
-        print ("full rows: ",fullRows)    # printing the full rows is done to visualize the process remove it afterwards
-        obst.removeFullRows(fullRows)
-        
-    counter += clock.get_time()* (1+(level/10)) #gets faster as by a 
-                
 
-     
+    if tetra.collides(bottom) or tetra.collides(obst):
+        for block in tetra.blocks:
+            if block.row <= 2:
+                inPlay = False
+        if inPlay:
+            tetra.moveUp()
+            obst.append(tetra)
+            obst.show()
+            tetra = Shape(MIDDLE - 1, 2, nextShapeNo)
+            shadow = Shape(MIDDLE - 1, 2, nextShapeNo, shadow=True)
+            oldShape = nextShapeNo
+            print(oldShape)
+            nextShapeNo = randint(1, 7)
+            nextShape = Shape(LEFT - 6, 3, nextShapeNo)
+            swap = False
+
+        fullRows = obst.findFullRows(
+            TOP, FLOOR, COLUMNS
+        )  # finds the full rows and removes their blocks from the obstacles
+        print(
+            "full rows: ", fullRows
+        )  # printing the full rows is done to visualize the process remove it afterwards
+        obst.removeFullRows(fullRows)
+
+    counter += clock.get_time() * (1 + (level / 10))  #gets faster as level goes up
+
+
     redrawScreen()
     clock.tick(10000)
     pygame.time.delay(30)
-    
+
 pygame.quit()
-    
-    
